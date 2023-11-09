d89e28f6cbc940a54edf27854cdb282355bf5736
Paras2322 <parasbagga23@Gmail.com>
2023-11-09 20:25:05 +0530

    Commit

---
+++ backend/cloudinary.js
@@ -0,0 +1,11 @@
+const cloudinary =  require ("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: "paras2322",
+  api_key: "279361125514313",
+  api_secret: "KI8nNctDnXq3dJkcWKCx2NvxB9Q",
+});
+
+module.exports= cloudinary;
+
+// p
